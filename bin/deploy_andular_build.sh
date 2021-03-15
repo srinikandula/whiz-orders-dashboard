023565ff4a5cbfe00f5fdcce75e91c750f8d7d5a
srinikandula <srini.kandula@whizzard.in>
2021-03-15 14:22:29 +0530

    Adding deployment script

--- bin/deploy_andular_build.sh
+++ bin/deploy_andular_build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 #
-# This script will perform a mongo dump of the local database and uploads the dump in to s3://mybus-mongo-dump-uploads.
-# The dump will be placed in a subdirectory named after the current date and time.
-#
-# Usage:  ./mongo_backup.sh
 #
 
 set -ex
 
 # to retain all backups, set this value to a non-positive integer
 bucket=whiz-orders-angular-builds
-
+#get the last uploaded zip filename
 build_name=`aws s3 ls s3://whiz-orders-angular-builds | tail -1 | awk '{print $4}'`
+
+#get the file name from .zip file
 filename="${build_name%%.*}"
+
+#copy the file from s3 bucket to local
 aws s3 cp s3://whiz-orders-angular-builds/$build_name .
+
+#unzip the file
 unzip $build_name -d ~/temp
+
+#copy to the build location
 mv ~/temp/$filename/architectui-angular-pro  ~/whiz-orders-angular-build
 
 rm -fR ~/temp
