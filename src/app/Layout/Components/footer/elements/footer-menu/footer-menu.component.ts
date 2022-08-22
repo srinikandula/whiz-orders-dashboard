a14e5f9693f7c50f8a902cc958e5943d16fba8c2
whizzard <kolli.durgaprasad8000@gmail.com>
2022-08-22 14:52:12 +0530

    Remove Client User Id

--- src/app/Layout/Components/footer/elements/footer-menu/footer-menu.component.ts
+++ src/app/Layout/Components/footer/elements/footer-menu/footer-menu.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-footer-menu',
   templateUrl: './footer-menu.component.html'
 })
 export class FooterMenuComponent implements OnInit {
-
-  constructor() { }
+  date = new Date();
+  currentYear: any = new Date();
+  constructor(private datePipe: DatePipe) { }
 
   ngOnInit() {
+    console.log(this.date);
+    this.currentYear = this.datePipe.transform(this.date, 'yyyy');
+    console.log(this.currentYear, '=======');
   }
 
 }
